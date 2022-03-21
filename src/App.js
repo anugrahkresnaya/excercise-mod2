89f3da3232234ef85dd581bb2ba605ad4b30d7aa
anugrahkresnaya <anugrahkresnaya@gmail.com>
2022-03-21 19:59:04 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Search from './components/Search';
+import Button from './components/Button';
 
 function App() {
+
+  const img = <img src='https://media.giphy.com/media/Vh8pbGX3SGRwFDh3V0/source.gif' alt="gif" />
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='Input'>
+          <Search />
+          <Button />
+        </div>
+        {img}
+        
       </header>
     </div>
   );
